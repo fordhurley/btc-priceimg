19be1e2310bf83ddd3b91b0a8cb8578c7b574cb5
Ford Hurley <ford.hurley@gmail.com>
2014-02-23 21:24:09 -0500

    Update copyright

--- priceimg/__init__.py
+++ priceimg/__init__.py
@@ -1,7 +1,5 @@
 """
-priceimg.py
-
-Copyright (C) 2013 Ford Hurley
+Copyright (C) 2014 Ford Hurley
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
