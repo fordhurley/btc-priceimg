b6a947227d6c28b497d4005a21d9b153d78b8726
Ford Hurley <ford.hurley@gmail.com>
2014-02-23 21:08:17 -0500

    Add test task to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,17 @@
-from fabric.api import local
+from fabric.api import local, shell_env
+
+
+def test():
+    """
+    Run test suite.
+    """
+    with shell_env(FLASK_ENV='test'):
+        local('nosetests')
 
 
 def deploy():
+    """
+    Deploy app to Heroku.
+    """
+    test()
     local('git push heroku master')
